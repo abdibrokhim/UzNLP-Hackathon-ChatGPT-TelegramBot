9c0603f9ca603295aa1330652c3c997599617991
abdibrokhim <abdibrokhim@gmail.com>
2022-12-24 03:12:56 +0500

    add translator

---
+++ copilot.py
@@ -0,0 +1,55 @@
+import os
+import openai
+import json
+from text_to_speech import TextToSpeech
+from dotenv import load_dotenv
+
+
+class Copilot:
+    GET_IN_UZBEK = " provide answer in Uzbek language"
+
+
+    def clear_text(self, text):
+        a = text.replace("\n", " ")
+        b = a.split()
+        c = " ".join(b)
+
+        return c
+
+
+    def get_answer(self, question):
+        prompt = question + self.GET_IN_UZBEK
+        
+        load_dotenv()
+
+        openai.api_key = os.getenv("CHAT_GPT3_API_KEY1")
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=512,
+            temperature=0.5,
+        )
+
+        json_object = response
+
+        # Convert the JSON object to a JSON string
+        json_string = json.dumps(json_object)
+
+        # Parse the JSON string using json.loads()
+        parsed_json = json.loads(json_string)
+
+        text = parsed_json['choices'][0]['text']
+        cleared_text = self.clear_text(text)
+        
+        return cleared_text
+
+
+# Usage:
+copilot = Copilot()
+a = copilot.get_answer("Birinchi Jahon urishini kim boshlagan?")
+
+print(a)
+
+# Usage:
+tts = TextToSpeech()
+tts.to_speech(a)
---
+++ text_to_speech.py
@@ -0,0 +1,21 @@
+import requests
+
+
+class TextToSpeech:
+    API_URL = "http://v2.nutq.uz/api/v1/cabinet/gen/?text="
+    PATH_TO_AUDIO = "audios/"
+    COUNTER = 0
+
+    def to_speech(self, text):
+        text = text.replace(" ", "%20")
+        response = requests.get(self.API_URL + text)
+        try:
+            with open(self.PATH_TO_AUDIO + f'audio{self.COUNTER}.mp3', "wb") as f:
+                f.write(response.content)
+            self.COUNTER += 1
+        except:
+            print("Network error!")
+
+# Usage:
+# tts = TextToSpeech()
+# tts.to_speech("Salom Dunyo!")
